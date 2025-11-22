9e0037ff0ed4fb9e77c27b575ceaffd8ec2f0c32
Soulaymane01 <soulaynadi@gmail.com>
2025-11-22 21:55:52 +0100

    resolved the vite.config.ts issue regarding deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,22 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-export default {
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [
+    react(),
+    mode === "development" && componentTagger(),
+  ].filter(Boolean),
+
+  // 👇 Add the base option HERE
   base: "/Soulaymane01/",
-};
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
